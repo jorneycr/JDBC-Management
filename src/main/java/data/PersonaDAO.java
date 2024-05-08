34c095fe543702a899d7d6e86454317f39d4067e
jorneycr <jorneycr@hotmail.com>
2024-05-07 22:32:26 -0600

    Some logic

---
+++ src/main/java/data/PersonaDAO.java
@@ -0,0 +1,49 @@
+package data;
+
+import domain.Persona;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaDAO {
+    private static final String SQL_SELECT = "SELECT * FROM persona";
+
+    public List<Persona> seleccionar(){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+
+                persona = new Persona(idPersona, nombre, apellido, email, telefono);
+                personas.add(persona);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+                throw new RuntimeException(e);
+            }
+        }
+
+        return  personas;
+    }
+}
