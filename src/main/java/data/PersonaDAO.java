2f63cc34413cf9954115fa6b11ef5a02725db958
jorneycr <jorneycr@hotmail.com>
2024-05-09 23:08:21 -0600

    insert and update

--- src/main/java/data/PersonaDAO.java
+++ src/main/java/data/PersonaDAO.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class PersonaDAO {
     private static final String SQL_SELECT = "SELECT * FROM persona";
+    private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES (?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE id_persona = ?";
 
     public List<Persona> seleccionar(){
         Connection conn = null;
@@ -47,4 +49,65 @@ public class PersonaDAO {
         return  personas;
     }
 
+    public int insertar(Persona persona){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getCorreo());
+            stmt.setString(4, persona.getTelefono());
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+                throw new RuntimeException(e);
+            }
+        }
+        return registros;
+    }
+
+    //actualizar
+    public int actualizar(Persona persona){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getCorreo());
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona());
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+                throw new RuntimeException(e);
+            }
+        }
+        return registros;
+    }
+
+    //eliminar
 }
--- src/main/java/test/TestManejoPersonas.java
+++ src/main/java/test/TestManejoPersonas.java
@@ -12,8 +12,25 @@ public class TestManejoPersonas {
         for(Persona persona: personas){
             System.out.println(persona);
         }
-        personas.forEach( persona -> {
-            System.out.println(persona);
+//        personas.forEach( persona -> {
+//            System.out.println(persona);
+//        });
+
+        //insertar persona
+//        Persona personaNuevo = new Persona("Mario", "Alfaro", "car@ghg.com", "8554411");
+//        int afectados = personaDAO.insertar(personaNuevo);
+//        System.out.println("Campos afectados "+ afectados);
+//        List<Persona> personasNuevas = personaDAO.seleccionar();
+//        personasNuevas.forEach( p -> {
+//            System.out.println(p);
+//        });
+
+        //update persona
+        Persona personaNuevo = new Persona(4,"Mario", "podero", "Mario@ghg.com", "8554411");
+        personaDAO.actualizar(personaNuevo);
+        List<Persona> personasNuevas = personaDAO.seleccionar();
+        personasNuevas.forEach( p -> {
+            System.out.println(p);
         });
     }
 }
